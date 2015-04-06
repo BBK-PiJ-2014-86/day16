406741b0c868e228734700be7b69de1ced73ee03
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2015-04-06 17:21:14 +0100

    Start of exercise 1
    Created the file and path

---
+++ Exercise1.java
@@ -0,0 +1,19 @@
+package day16;
+
+import java.io.File;
+
+public class Exercise1 {
+	
+	private final static String PATH ="/users/ageorgiev/documents/workspace/uni"; 
+
+	public static void main(String[] args) {
+
+		File directory = new File (PATH);
+		
+		
+	 
+		
+		
+	}
+
+}
