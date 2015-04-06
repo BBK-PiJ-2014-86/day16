8d307fb39240b6e8aaa794c77cf103fd5d0ad349
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2015-04-06 17:37:10 +0100

    finish 1 exercise
    displays the list of files in the desktop of the user

--- Exercise1.java
+++ Exercise1.java
@@ -4,13 +4,17 @@ import java.io.File;
 
 public class Exercise1 {
 	
-	private final static String PATH ="/users/ageorgiev/documents/workspace/uni"; 
+	private final static String PATH = System.getProperty("user.home")+ File.separator +"desktop";
 
 	public static void main(String[] args) {
 
 		File directory = new File (PATH);
 		
+		String [] fileArray = directory.list();
 		
+		for (String s: fileArray) {
+			System.out.println(s);
+		}
 	 
 		
 		
